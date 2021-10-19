c65d7dbc2f4d78d1e43a551cb9b009c8dd9eaf3a
R2d2cruz <51866042+R2d2cruz@users.noreply.github.com>
2021-10-19 11:30:12 -0500

    finish homework
    add fact using unsigned long long
    add mysin complete function
    fix my stupid error

--- plantillahw.cpp
+++ plantillahw.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 
 double mysin(double x, int N);
+unsigned long long fact(int i);
 
 int main() 
 {
@@ -13,16 +14,38 @@ int main()
     const double x = M_PI / 3;
     const double exact = std::sin(x);
 
-    for (int NMAX = 0; NMAX < 1000; NMAX++)
+    for (int NMAX = 10; NMAX < 20; NMAX++)
     {
-        double diff = std::fabs((mysin(x, NMAX), - exact) / exact);
+        double diff = std::fabs(mysin(x, NMAX) - exact) / exact;
         std::cout << NMAX << "\t" << diff << "\n";
     }
     
     return 0;
 }
 
+unsigned long long fact(int i){
+	if (i <= 1) {
+        return 1;
+    } else {
+        return i * fact(i - 1);
+    }
+}
+
 double mysin(double x, int N)
 {
-    return 0;
+    double dividend = 0;
+    double quotient = 0;
+    int base = 0;
+    double outcome = 0;
+    for (int n = 1; n < N; n++)
+    {
+        base = (2 * n) - 1;
+        dividend = fact(base);
+        quotient = (pow(-1,n-1)) * (pow(x, (double) base));
+        /* std::cout << "base: " << base << ", fact: " << fact(base) << ", dividen: " << dividend << ", quotient: " << quotient << "\n";*/
+        outcome += quotient / dividend;
+    }
+    /* std::cout << outcome << "\n"; */
+    
+    return outcome;
 }
