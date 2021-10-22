0440ed87823e83c4ea570685cf1db3a94b1bbd4f
R2d2cruz <51866042+R2d2cruz@users.noreply.github.com>
2021-10-22 11:34:41 -0500

    add sumUp y sumDown

--- makefile-primera-clase/sumas.cpp
+++ makefile-primera-clase/sumas.cpp
@@ -2,10 +2,30 @@
 #include <iostream>
 
 
-
+typedef float REAL;
+REAL sumUp(int N);
+REAL sumDown(int N);
 
 
 int main(void) {
 
     return 0;
+}
+
+REAL sumUp(int N) {
+    REAL suma = 0;
+    for (int ii = 1; ii <= N; ii++)
+    {
+        suma += 1/ii;
+    }
+    
+}
+
+REAL sumDown(int N) {
+    REAL suma = 0;
+    for (int ii = N; ii >= 1; ii--)
+    {
+        suma += 1/ii;
+    }
+    
 }
